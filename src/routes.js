b92c96af0460da643b9113963e564d35b2861f45
wagner <wagnerricardonet@gmail.com>
2019-03-13 22:38:16 -0300

    sequelize rodando

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 const routes = require("express").Router();
 
-routes.get("/", (req, res) => {
-  res.send("hellou");
+const { User } = require("./app/models");
+
+routes.get("/", async (req, res) => {
+  const user = await User.create({
+    name: "wagner",
+    email: "wagner@testes.com",
+    password_hash: "1244"
+  });
+
+  return res.json({ user });
 });
 
 module.exports = routes;
